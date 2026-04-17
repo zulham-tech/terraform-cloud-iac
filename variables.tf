c0bdb91d78497b964e3c912796e9e9a21537cebe
zulhamdan-tech <zulham.va@gmail.com>
2026-04-17 18:51:50 +0700

    feat: add input variables

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region"    { default = "ap-southeast-1" }
+variable "gcp_project_id" { description = "GCP Project ID" }
+variable "gcp_region"    { default = "asia-southeast1" }
+variable "environment"   { default = "dev" }
+variable "project_name"  { default = "zulham-tech-data" }
